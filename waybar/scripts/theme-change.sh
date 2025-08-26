28d569382ae0e7a618201b3dd3f7b75967154524
codenewbird <824392528@qq.com>
2025-08-26 23:53:35 +0800

    dotfile update

--- waybar/scripts/theme-change.sh
+++ waybar/scripts/theme-change.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/bash
 
-if test -s ./curtheme 
+curthemefile=~/.config/waybar/scripts/curtheme
+
+if test -s $curthemefile 
 then
-    curtheme=$(cat ./curtheme)
+    curtheme=$(cat $curthemefile)
     rm $HOME/.config/waybar/colors/color.css;
     if [ "$curtheme" == "dark" ]
     then
         ln -s $HOME/.config/waybar/colors/primary.css $HOME/.config/waybar/colors/color.css
-        echo "primary" > ./curtheme
+        echo "primary" > $curthemefile
     else
         ln -s $HOME/.config/waybar/colors/dark.css $HOME/.config/waybar/colors/color.css
-        echo "dark" > ./curtheme
+        echo "dark" > $curthemefile
     fi
 else
-    echo "primary" > ./curtheme
+    echo "primary" > $curthemefile
     ln -s $HOME/.config/waybar/colors/primary.css $HOME/.config/waybar/colors/color.css
 fi
     
